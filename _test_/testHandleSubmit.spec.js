cd3404ee985581bbc1939a3b6e80c3c0bc432120
Necib Mohamed <75915496+mln95@users.noreply.github.com>
2021-03-24 23:36:49 +0100

    Add files via upload

---
+++ _test_/testHandleSubmit.spec.js
@@ -0,0 +1,6 @@
+import {handleSubmit_first} from '../src/client/js/handleSubmit_first'
+
+describe("Testing the submit functionality", () => {  
+  test("Testing the handleSubmit() function", () => {
+         expect(handleSubmit_first).toBeDefined();
+})});
